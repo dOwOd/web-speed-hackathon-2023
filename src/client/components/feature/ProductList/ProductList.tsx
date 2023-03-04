2ca4851e7329fd55d5a63fb908cfeb3df12974c0
sksn <ps.net26@gmail.com>
2023-03-04 18:29:12 +0900

    dynamic importに変更

--- src/client/components/feature/ProductList/ProductList.tsx
+++ src/client/components/feature/ProductList/ProductList.tsx
@@ -1,29 +1,37 @@
 import type { FC } from 'react';
 import { memo } from 'react';
+import { Suspense, lazy } from "react";
 
 import type { FeatureSectionFragmentResponse } from '../../../graphql/fragments';
 import { DeviceType, GetDeviceType } from '../../foundation/GetDeviceType';
-import { ProductGridList } from '../ProductGridList';
-import { ProductListSlider } from '../ProductListSlider';
 
 type Props = {
   featureSection: FeatureSectionFragmentResponse;
 };
 
+const ProductListSlider = lazy(() =>
+  import('../ProductListSlider').then(({ ProductListSlider }) => ({ default: ProductListSlider }))
+)
+const ProductGridList = lazy(() =>
+  import('../ProductGridList').then(({ ProductGridList }) => ({ default: ProductGridList }))
+)
+
 export const ProductList: FC<Props> = memo(({ featureSection }) => {
   return (
-    <GetDeviceType>
-      {({ deviceType }) => {
-        switch (deviceType) {
-          case DeviceType.DESKTOP: {
-            return <ProductListSlider featureSection={featureSection} />;
+    <Suspense fallback={<div>Loading...</div>}>
+      <GetDeviceType>
+        {({ deviceType }) => {
+          switch (deviceType) {
+            case DeviceType.DESKTOP: {
+              return <ProductListSlider featureSection={featureSection} />;
+            }
+            case DeviceType.MOBILE: {
+              return <ProductGridList featureSection={featureSection} />;
+            }
           }
-          case DeviceType.MOBILE: {
-            return <ProductGridList featureSection={featureSection} />;
-          }
-        }
-      }}
-    </GetDeviceType>
+        }}
+      </GetDeviceType>
+    </Suspense>
   );
 });
 
